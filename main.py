6715cf711a385769a18eda2f3d1c77dead0ce4fc
Екатерина <zhemkaterina@yandex.ru>
2021-11-18 17:39:14 +0300

    функция с двумя print

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'my first git program')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print('my second git program')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
